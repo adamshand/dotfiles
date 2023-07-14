cf44aea4e3d1c6a57c707cc6e2e599289d85f950
Adam Shand <adam@shand.net>
2023-07-15 08:13:54 +1200

    Update bin/noarch/backup-mikrotik.sh

--- bin/noarch/executable_backup-mikrotik.sh
+++ bin/noarch/executable_backup-mikrotik.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 MIKROTIK="172.16.1.38 172.16.1.39"
+TMPFILE="$(mktemp)"
 FOLDER="/Users/adam/Documents/Backups/Mikrotik"
 #TODAY="$(date +%Y-%m-%d)"
 TODAY="$(date +%a)"
 SSH_KEY="~adam/.ssh/id_rsa"
 
+trap 'rm -f ${TMPFILE}; exit 1' 1 2 15
+
 for router in $MIKROTIK; do
-  ssh -i $SSH_KEY adam@${router} export > ${FOLDER}/${router}-${TODAY}.txt
+  ssh -i $SSH_KEY adam@${router} export > $TMPFILE
+  
+  if [ -s $TMPFILE ]; then
+    mv $TMPFILE ${FOLDER}/${router}-${TODAY}.txt
+  fi
 done
+
