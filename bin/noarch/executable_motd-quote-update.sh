2734270a352e23a9d0cd4004344ec08456b9c5de
Adam Shand <adam@shand.net>
2024-01-19 19:34:05 +1300

    Update bin/noarch/motd-quote-update.sh

--- bin/noarch/executable_motd-quote-update.sh
+++ bin/noarch/executable_motd-quote-update.sh
@@ -3,7 +3,7 @@
 # Written 19 Jan 2024
 
 #QUOTE_URL="http://172.16.1.42:5173/api/random/quote"
-QUOTE_URL="https://adam.nz/api/random/quote"
+QUOTE_URL="https://adam.nz/api/quote?id=random"
 BANNER_FILE="/etc/issue.net"
 
 if [ "$1" == "debug" ]; then
@@ -15,7 +15,7 @@ fi
 set -o pipefail
 if ! QUOTE=$(curl --silent --fail "$QUOTE_URL" | \
     # decoding entites in api now #python3 -c "import html, sys; print(html.unescape(sys.stdin.read()))" | \
-    fmt -w 80 | \
+    fmt -w 60 | \
     boxes -p h2v1 -d stone -a r); then
   echo "error: downloading and formatting quote" 1>&2
   exit 1
