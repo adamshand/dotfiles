4d699f07994f62455eec754d2cedc1b80fec029d
Adam Shand <adam@shand.net>
2024-01-19 16:25:39 +1300

    Update bin/noarch/motd-quote-update.sh

--- bin/noarch/executable_motd-quote-update.sh
+++ bin/noarch/executable_motd-quote-update.sh
@@ -2,11 +2,17 @@
 # Get random quote, format and update /etc/issue.net for SSH banner
 # Written 19 Jan 2024
 
-QUOTE_URL="http://172.16.1.42:5173/api/random/quote"
+#QUOTE_URL="http://172.16.1.42:5173/api/random/quote"
+QUOTE_URL="https://adam.nz/api/random/quote"
 BANNER_FILE="/etc/issue.net"
 
-set -o pipefail
+if [ "$1" == "debug" ]; then
+  DEBUG="yes"
+  echo "DEBUG: on" 1>&2
+fi
 
+# sets final exit code as failure if any step in pipeline fails
+set -o pipefail
 if ! QUOTE=$(curl --silent --fail "$QUOTE_URL" | \
     python3 -c "import html, sys; print(html.unescape(sys.stdin.read()))" | \
     fmt -w 80 | \
@@ -15,9 +21,13 @@ if ! QUOTE=$(curl --silent --fail "$QUOTE_URL" | \
   exit 1
 fi
 
-if [ ! -w "$BANNER_FILE" ]; then
+if [ ! -w "$BANNER_FILE" -a -z "$DEBUG" ]; then
   echo "error: can't write to $BANNER_FILE" 1>&2
   exit 1
 fi
 
-echo -e "\n$QUOTE\n" > $BANNER_FILE
+if [ -n "$DEBUG" ]; then
+  echo -e "\n$QUOTE\n"
+else
+  echo -e "\n$QUOTE\n" > $BANNER_FILE
+fi
