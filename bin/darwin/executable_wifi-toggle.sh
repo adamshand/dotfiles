52d093901ad4c731e96a730fbbe7c24f18cce180
Adam Shand <adam@shand.net>
2024-02-07 08:11:19 +1300

    Add bin/darwin/wifi-toggle.sh

---
+++ bin/darwin/executable_wifi-toggle.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Automatically toggle macOS Wi-Fi based on ethernet status using launchd.
+# Written by Adam Shand <adam@shand.net> on 7 Feb 2024
+
+PATH="/bin:/sbin:/usr/bin:/usr/sbin"
+
+LAUNCHD_SERVICE="com.haume.wifi-toggle"
+SERVICE="${HOME}/Library/LaunchAgents/${LAUNCHD_SERVICE}.plist"
+
+# asdadf
+ETHERNET_REGEX="CalDigit TS3"
+# ETHERNET_REGEX="Apple USB Ethernet Adapter"
+WIFI_REGEX="(Wi-Fi|Airport)"
+
+print_usage() {
+  echo "Automatically toggle macOS Wi-Fi based on ethernet status (uses launchd)"
+  echo "Usage: $(basename $0) [ off | debug | help ]"
+  exit 0
+}
+
+print_error() {
+  echo -e "ERROR: $1" >&2
+  exit 1
+}
+
+print_debug() {
+  test -n "$DEBUG" && echo -e "DEBUG: $1" >&2
+}
+
+send_notification() {
+  osascript -e "display notification \"by $(basename $0)\" with title \"$1\""
+}
+
+is_launchd_loaded() {
+  if launchctl list | grep -q "$LAUNCHD_SERVICE"; then
+    print_debug "is_launchd_loaded(): $LAUNCHD_SERVICE already loaded"
+    return 0
+  else
+    print_debug "is_launchd_loaded(): $LAUNCHD_SERVICE not loaded"
+    return 1
+  fi
+}
+
+enable_launchd() {
+  echo "Creating launchd service: $SERVICE"
+  cat <<EOF > "$SERVICE"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>${LAUNCHD_SERVICE}</string>
+  <key>RunAtLoad</key>
+  <true/>
+  <key>ProgramArguments</key>
+  <array>
+    <string>$(realpath "$0")</string>
+  </array>
+  <key>WatchPaths</key>
+  <array>
+    <string>/Library/Preferences/SystemConfiguration</string>
+  </array>
+</dict>
+</plist>
+EOF
+  echo "Loading launchd service: $LAUNCHD_SERVICE"
+  launchctl load "$SERVICE"
+}
+
+disable_launchd() {
+  echo "Unloading launchd service: $LAUNCHD_SERVICE"
+  launchctl unload "${HOME}/Library/LaunchAgents/${LAUNCHD_SERVICE}.plist"
+  rm "${HOME}/Library/LaunchAgents/${LAUNCHD_SERVICE}.plist"
+  exit 0
+}
+
+get_interface() {
+  test -z "$1" && print_error "get_interface(): no regex provided"
+  INTERFACE=$(networksetup -listnetworkserviceorder | grep -E -A 1 "$1" | grep -E -o "en[0-9]+")
+
+  if [ -z "$INTERFACE" ]; then
+    print_error "No Ethernet interface found with regex: $1"
+  elif [[ "$INTERFACE" == *$'\n'* ]]; then
+    print_error "Multiple Ethernet interfaces found with regex: $1"
+  fi
+
+  print_debug "get_interface(): regex '$1' -> interface '$INTERFACE'"
+  echo "$INTERFACE"
+}
+
+is_interface_active() {
+  test -z "$1" && print_error "is_interface_active(): no interface provided"
+
+  if ifconfig "$1" 2>&1 | grep -q "status: active"; then
+    echo -n "active"
+    return 0
+  else
+    echo -n "inactive"
+    return 1
+  fi
+}
+
+if [ "${OSTYPE:0:6}" != "darwin" ]; then
+  print_error "This script only runs on macOS"
+elif [ "$1" == "help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    print_usage
+elif [ "$1" == "off" ]; then
+  disable_launchd
+elif [ "$1" == "debug" ]; then
+  DEBUG="yes"
+  print_debug "Debugging enabled"
+fi
+
+if ! is_launchd_loaded; then
+  enable_launchd
+fi
+
+ETHERNET_INTERFACE=$(get_interface "$ETHERNET_REGEX")
+WIFI_INTERFACE=$(get_interface "$WIFI_REGEX")
+
+ETHERNET_STATUS=$(is_interface_active "$ETHERNET_INTERFACE")
+WIFI_STATUS=$(is_interface_active "$WIFI_INTERFACE")
+print_debug "ethernet status: '$ETHERNET_STATUS', wifi status: '$WIFI_STATUS'"
+
+if [ "$ETHERNET_STATUS" == "active" ] && [ "$WIFI_STATUS" == "active" ]; then
+  print_debug "disabling wifi"
+  networksetup -setairportpower "$WIFI_INTERFACE" off
+  send_notification "Wi-Fi Disabled"
+elif [ "$ETHERNET_STATUS" == "inactive" ] && [ "$WIFI_STATUS" == "inactive" ]; then
+  print_debug "enabling wifi"
+  networksetup -setairportpower "$WIFI_INTERFACE" on
+  send_notification "Wi-Fi Enabled"
+else
+  print_debug "no change to wifi"
+fi
