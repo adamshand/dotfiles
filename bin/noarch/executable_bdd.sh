579e94fc11741713b4c8f911b610b6b1294b5b4d
Adam Shand <adam@shand.net>
2024-03-06 22:12:28 +1300

    Update bin/noarch/bdd.sh

--- bin/noarch/executable_bdd.sh
+++ bin/noarch/executable_bdd.sh
@@ -21,15 +21,14 @@ DEBUG="yes"
 #DEBUG=""
 
 DATABASE_REGEX="(db|mysql|mariadb|postgres)"
-# EXCLUDE_MOUNT_REGEX="/srv/srv/www /vol/media/moa"
-EXCLUDE_MOUNT_REGEX="^(/srv|srv/www|/vol/moa/media|/vol/moa/media/[a-z]*)$"
+EXCLUDE_MOUNT_REGEX="^(/srv|srv/www|/vol/moa/media(/[a-z].*)?)$"
 EXCLUDE_SQLITE_REGEX="^(.*users.*|.*\.bak|.*\.old)$"
 
 if [ "$EUID" -ne 0 ]; then
   print_error "must be run as root"
 fi
 
-for cmd in docker sqlite3; do
+for cmd in docker install sqlite3; do
   if ! command -v $cmd &> /dev/null; then
     print_error "$cmd command, not found"
   fi
@@ -78,67 +77,83 @@ sqlite_backup_file() {
   local slug="sqlite-${slug_raw//\//#}"
   
   # remove `.replica#.swarm_id` from end of container name to have stable backup dir
-  local backup_dest="${BACKUP_BASE}/${container//\.[0-9]*\.[A-Za-z0-9]*/}/${DATESTAMP}"
+  local backup_dest="${BACKUP_BASE}/${CONTAINER_SHORT}/${DATESTAMP}"
   make_backup_dest ${backup_dest}
 
   local file_dest="${backup_dest}/${slug}-${TIMESTAMP}"
 
-  # TODO: some kind of retry on locking errors?
-  # eg. sql error: database is locked (5)
-
-  echo "backup: ${file_dest}.backup.gz"
-  $SQLITE $file_src ".backup ${file_dest}.backup" && gzip -9qf "${file_dest}.backup"
+  #echo "backup: ${file_dest}.backup.gz"
+  #$SQLITE $file_src ".backup ${file_dest}.backup" && gzip -9qf "${file_dest}.backup"
 
   echo "dump: ${file_dest}.dump.gz"
   $SQLITE $file_src ".dump" | gzip -9 > "${file_dest}.dump.gz"
+}
+
+cleanup_backups() {
+  # Delete backups more than $DAYS_TO_KEEP days old
+  echo -e "\n## DELETING OLD BACKUPS"
+  find "$BACKUP_BASE" -mindepth 1 -maxdepth 2 -mtime +${DAYS_TO_KEEP} -name "[0-9]{4}-[0-9]{2}-[0-9]{2}" -print -delete
+
+  # Delete empty directories (but not BACKUP_BASE)
+  echo -e "\n## DELETING EMPTY FOLDERS"
+  find "$BACKUP_BASE" -mindepth 1 -type d -empty -print -delete
+
+  echo -e "\n## CURRENT BACKUPS: ${BACKUP_BASE}\n"
+  tree --du -sh "$BACKUP_BASE"
+}
+
+## BACKUP POSTGRESQL DATABASES
+postgres_backup_container() {
+  local username=$( docker inspect "$container" | jq -r '.[0].Config.Env[] | select(startswith("POSTGRES_USER=")) | split("=")[1]' )
+  local password=$( docker inspect "$container" | jq -r '.[0].Config.Env[] | select(startswith("POSTGRES_PASSWORD=")) | split("=")[1]' )
+  local database=$( docker inspect "$container" | jq -r '.[0].Config.Env[] | select(startswith("POSTGRES_DB=")) | split("=")[1]' )
+  
+  DATABASES=$(
+    docker exec "$container" psql -U "$username" --tuples-only -P format=unaligned \
+      -c "SELECT datname FROM pg_database WHERE NOT datistemplate AND datname <> 'postgres'" 
+    )
+
+  for database in $DATABASES; do
+    echo "## database: $database (user: $username password: $password)"
+
+    local backup_dest="${BACKUP_BASE}/${CONTAINER_SHORT}/${DATESTAMP}"
+    make_backup_dest ${backup_dest}
+
+    local backup_file="${backup_dest}/${database}-${TIMESTAMP}.sql.gz"
+    echo "pg_dump: $backup_file"
+    docker exec "$container" pg_dump -U "$username" "$database" | gzip > $backup_file
+
+    local backup_file="${backup_dest}/${database}-${TIMESTAMP}.dump"
+    echo "pg_dump: $backup_file"
+    docker exec "$container" pg_dump -U "$username" --format=c "$database" > "${backup_file}"
+  done
+
 }
 
 ## BEGIN MAIN
 test -n "$DEBUG" && echo "DEBUG: $DEBUG" 1>&2
 
 if [ ! -d "$BACKUP_BASE" ]; then
-  if install -o root -g "$BACKUP_GROUP" -m 2750 -d $BACKUP_BASE; then
-   print_debug "created $BACKUP_BASE" 1>&2
-  else
+  if ! install -o root -g "$BACKUP_GROUP" -m 2750 -d $BACKUP_BASE; then
     print_error "cannot create $BACKUP_BASE" 1>&2
   fi
 fi
 
 for container in $(docker container ls --format "{{.Names}}"); do
   echo -e "\n### container: $container"
+  CONTAINER_SHORT=${container//\.[0-9]*\.[A-Za-z0-9]*/}
 
   # Match MySQL, MariaDB or PostgreSQL
-  if echo $container | grep -iEq "$DATABASE_REGEX"; then
-    print_debug "$container matches DATABASE_REGEX $DATABASE_REGEX"
-    
+  if echo $container | grep -iEq "db|postgis|postgres"; then
+    image=$( docker inspect "$container" --format '{{.Config.Image}}' )
+    echo "### image: $image"
+
+    postgres_backup_container
+
   else
-    sqlite_backup_container
+    # sqlite_backup_container
+    echo "skipping $container"
   fi
 done
 
-# Delete backups more than $DAYS_TO_KEEP days old
-echo -e "\n## DELETING OLD BACKUPS"
-find "$BACKUP_BASE" -mindepth 1 -maxdepth 2 -mtime +${DAYS_TO_KEEP} -name "[0-9]{4}-[0-9]{2}-[0-9]{2}" -print -delete
-
-# Delete empty directories (but not BACKUP_BASE)
-echo -e "\n## DELETING EMPTY FOLDERS"
-find "$BACKUP_BASE" -mindepth 1 -type d -empty -print -delete
-
-echo -e "\n## CURRENT BACKUPS: ${BACKUP_BASE}\n"
-tree --du -sh "$BACKUP_BASE"
-
-##############################
-### delete ...
-deleteme_sqlite_find() {
-  result=$( 
-    find ${1} -maxdepth ${SQLITE_SEARCH_DEPTH} -exec file {} \; | awk -F: '/SQLite 3.x database/ {print $1}' 
-  )
-
-  if [ -z "$result" ]; then
-    return 1
-  else
-    echo "$result"
-  fi
-}
-
-container_short="$( echo $container | sed 's/\.[0-9]\+\.[A-Za-z0-9]\+//g' )"
+cleanup_backups
