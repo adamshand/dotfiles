bcbd7a373eeb5ca0c775931b5e7a7c56784ce774
Adam Shand <adam@shand.net>
2024-04-25 16:47:41 +1200

    Update .config/iterm2/com.googlecode.iterm2.plist Update .config/fish/config.fish Update .zshrc

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,33 +4,35 @@ set fish_greeting ""
 fish_add_path -p ~/bin/noarch
 
 if status is-interactive
-  set -g myhostname (string split "." $hostname)[1]
-  set -g masterhost "chimera"
+  set -g myhostname   (string split "." $hostname)[1]
+  set -g masterhost   chimera
 
-  set -g debug "true"
-  if test $myhostname = "chimera"
+  set -g debug ""
+  if test $myhostname = "chimeraX"
       set -g debug "true"
   end
 
-  set -x BC_ENV_ARGS ~/.bc
-  set -x EDITOR vim
-  set -x EZA_ARG ""
-  set -x LESS -FXR
-  set -x PAGER less
-  set -x SUDO_PROMPT "Sudo Password: "
+  set -x BC_ENV_ARGS  ~/.bc
+  set -x EDITOR       vim
+  set -x EZA_ARG      "-F"
+  set -x EZAL_ARG     "-lh --sort=modified --time-style=long-iso"
+  set -x LESS         -FXR
+  set -x PAGER        less
+  set -x SUDO_PROMPT  "Sudo Password: "
 
   set platform (uname)
   if test $platform = "Darwin"
     test $debug; and echo "OS: Darwin"
+    set -g L_ARG  "-lhPO"
+    set -g PS_ARG "auxww"
     source ~/.config/fish/conf.d/darwin.include
-    set -g LARG "-lhPO"
-    set -g PSARG "auxww"
 
   else if test $platform = "Linux"
     test $debug; and echo "OS: Linux"
+    set -g L_ARG  "-lh"
+    set -g PS_ARG "-efww"
     source ~/.config/fish/conf.d/linux.include
-    set -g LARG "-lh"
-    set -g PSARG "-efww"
+
   else
     test $debug; and echo "OS: Unknown"
   end
@@ -38,19 +40,26 @@ if status is-interactive
   #########################################################
   ### ALIASES
 
-  abbr cp 'cp -vi'
+  abbr cp "cp -vi"
   abbr mv "mv -vi"
 
   # alias edpro "cme ~/.zshrc && cm re-add ~/.zshrc && exec zsh"
-  alias egrep "egrep --color=auto"
-  alias grep "grep --color=auto"
-  alias l "ls $LARG"
-  alias ls "ls -F"
-  alias mkdir "mkdir -p"
-  alias more "less -F"
-  alias nano "nano -zbx"
-  alias mkroot "sudo -E fish"
-  alias pwgen "bw generate -cp --words 2 --separator . --includeNumber | tee /dev/stderr | pbcopy"
+  alias egrep   "egrep --color=auto"
+  alias grep    "grep --color=auto"
+  alias l       "ls $L_ARG"
+  alias ls      "ls -F"
+  alias mkdir   "mkdir -p"
+  alias more    "less -F"
+  alias nano    "nano -zbx"
+  alias mkroot  "sudo -E fish"
+  alias pwgen   "bw generate -cp --words 2 --separator . --includeNumber | tee /dev/stderr | pbcopy"
+
+  if type -q eza
+    test debug; and echo "FOUND: eza"
+    set COMMON  "--git --git-repos --group --group-directories-first --hyperlink"
+    alias l   "eza $COMMON $EZAL_ARG"
+    alias ls  "eza $COMMON $EZA_ARG"
+  end
 
   #########################################################
   ## CHEZMOI
@@ -104,7 +113,8 @@ if status is-interactive
       set DCMD $argv[2]
     end
 
-    set -e argv[1..2]  # remove first and second arguments
+    # remove first and second arguments from $argv
+    set -e argv[1..2]
     docker exec -it $CONTAINER $DCMD $argv
   end
 
@@ -116,8 +126,7 @@ if status is-interactive
   end
 
   function gcp
-    git status -s
-    echo -n "Commit message: "
+    git status -s; and echo -n "Commit message: "
     read -l MSG
     and git pull
     and git commit -a -m "gcp: $MSG"
@@ -128,11 +137,11 @@ if status is-interactive
       mkdir -p $argv[1]; and cd $argv[1]
   end
 
-  function p -a process -d "Search for matching processes"
-    if test $process
-      ps $PSARG | grep -i "[^]]$process"
+  function p -a string -d "Search for matching processes"
+    if test $string
+      ps $PS_ARG | grep -i "[^]]$string"
     else
-      ps $PSARG | less
+      ps $PS_ARG | less
     end
   end
 
@@ -140,7 +149,7 @@ if status is-interactive
     if [ "$USER" = "adam" -a -x ssh.sh ]
         ssh.sh $argv
     else
-      echo "# using /usr/bin/ssh"
+      echo "# skipping ssh.sh (using ssh)"
       ssh $argv
     end
   end
@@ -154,26 +163,19 @@ if status is-interactive
   #########################################################
   ## PER-HOST OVERRIDES
 
-  if test "$myhostname" = "chimera"
-    if set -q debug
-      echo "HOST: chimera"
-    end
+  if test $myhostname = "chimera"
+    test $debug; and echo "HOST: chimera"
 
-  else if test "$myhostname" = "kauri"
-    if set -q debug
-      echo "HOST: kauri"
-    end
+  else if test $myhostname = "kauri"
+    test $debug; and echo "HOST: kauri"
 
     set -x RESTIC_PASSWORD_FILE "/home/adam/etc/restic.txt"
-    set -x RESTIC_REPOSITORY "/vol/obelix/restic/"
+    set -x RESTIC_REPOSITORY    "/vol/obelix/restic/"
 
-  else if test "$myhostname" = "beam" -o "$HOST_SHORT" = "ch-test1"
-    if set -q debug
-      echo "HOST: beam or ch-test1 (miro)"
-    end
+  else if test $myhostname = "beam"; or test $myhostname = "ch-test1"
+    test $debug; and echo "HOST: beam or ch-test1"
     functions -e mkroot
   end
-
 
 end #is-interactive
 
